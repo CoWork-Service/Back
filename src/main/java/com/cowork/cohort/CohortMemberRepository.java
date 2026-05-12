3f74dfd52f0327cc5f70b8cfcdf32aea42a7bca7
CheonWoong-Park <cheonwoong21@naver.com>
2026-05-12 16:21:04 +0900

    Fetch users with cohort members

--- src/main/java/com/cowork/cohort/CohortMemberRepository.java
+++ src/main/java/com/cowork/cohort/CohortMemberRepository.java
@@ -1,17 +1,24 @@
 package com.cowork.cohort;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CohortMemberRepository extends JpaRepository<CohortMember, Long> {
 
+    @EntityGraph(attributePaths = "user")
     List<CohortMember> findByCohortId(Long cohortId);
 
+    @EntityGraph(attributePaths = "user")
     Optional<CohortMember> findByCohortIdAndUserId(Long cohortId, Long userId);
 
     boolean existsByCohortIdAndUserId(Long cohortId, Long userId);
 
     List<CohortMember> findByUserId(Long userId);
+
+    @Override
+    @EntityGraph(attributePaths = "user")
+    Optional<CohortMember> findById(Long id);
 }
